5f1790a24a1544c08bd04f7b3d86982a379ecc48
fanagnou <fanagnou.m@gmail.com>
2018-12-19 01:57:26 +0100

    update LSTM

--- src/LSTM.py
+++ src/LSTM.py
@@ -5,19 +5,22 @@ import time
 import text_representation as text_repr
 import numpy as np
 
+from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv1D, Embedding, Activation, Flatten, GlobalMaxPooling1D, LSTM
 from keras.optimizers import Adam, SGD
-from sklearn.model_selection import train_test_split
 
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 start_time = time.time()
 
-full_dataset = False
+full_dataset = True
 glove_pretrained = True
 
 train_data, y, test_data = helpers.get_processed_data(full_dataset=full_dataset)
@@ -48,6 +51,23 @@ model.add(LSTM(units=32, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(2, activation='softmax'))
 
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-X_train, X_test, y_train, y_test = train_test_split(train_data_seq, y, test_size=0.2, random_state=42)
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=32, verbose=1)
 
+X_train, X_test, y_train, y_test = train_test_split(train_data_seq, y, test_size=0.2, random_state=42)
+
+filepath_acc = "../data/intermediate/lstm1.weights.best.hdf5"
+checkpoint_acc = ModelCheckpoint(filepath_acc, monitor='val_acc', verbose=2, save_best_only=True, mode='max')
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=32, verbose=1, callbacks=[checkpoint_acc])
+
+
+best_model = load_model("../data/intermediate/lstm1.weights.best.hdf5")
+
+helpers.predict_and_save(best_model, test_data_seq, 'CNN')
+
+scores = []
+for i in range(1,4):
+    X_train, X_test, y_train, y_test = train_test_split(train_data_seq, y, test_size=10000, random_state=i)
+    score = best_model.evaluate(X_test,y_test,verbose=1)
+    print("%s: %.2f%%" % (best_model.metrics_names[1], score[1]*100))
+    scores.append(score[1])
+print('Average score:', sum(scores)/len(scores))
